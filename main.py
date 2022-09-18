026b163f001d1e94ace767c69d8c6f6e664b0526
Leonid <Anevskyi2018@yandex.ru>
2022-09-18 22:51:15 +0400

    add sleep func

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 
+headers = {"User-Agent":"Mozilla/5.0"}
 
-headers = {
-    'User-Agent': ('Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 '
-                   'Firefox/14.0.1'),
-    'Accept':
-        'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language':
-        'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
-    'Accept-Encoding':
-        'gzip, deflate',
-    'Connection':
-        'keep-alive',
-    'DNT':
-        '1'
-}
+# headers = {
+#     'User-Agent': ('Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 '
+#                    'Firefox/14.0.1'),
+#     'Accept':
+#         'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#     'Accept-Language':
+#         'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
+#     'Accept-Encoding':
+#         'gzip, deflate',
+#     'Connection':
+#         'keep-alive',
+#     'DNT':
+#         '1'
+# }
 
 if not os.path.isdir("dataset"):
     os.mkdir("dataset")
@@ -36,7 +38,7 @@ def download_image(url_image, name, item):
 
 
 def get_image_url(item):
-    for page in range(1, 40):
+    for page in range(1, 35):
         url = f'https://yandex.ru/images/search?p={page}&text={item}'
         src = requests.get(url, headers=headers)
         soup = BeautifulSoup(src.text, "lxml")
@@ -46,15 +48,20 @@ def get_image_url(item):
             yield url_img
 
 
-number = 1
+number = 0
 for url_of_item in get_image_url("rose"):
+    number += 1
     download_image(url_of_item, str(number).zfill(4), "rose")
-    number += 1
+    print(1)
+    sleep(1)
 
-number = 1
+number = 0
 for url_of_item in get_image_url("tulip"):
-    download_image(url_of_item, str(number).zfill(4), "tulip")
     number += 1
+    download_image(url_of_item, str(number).zfill(4), "tulip")
+    print(2)
+    sleep(1)
+
 
 
 
